468859f3a35d9972696578bb900b3bbf506b5cf0
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2023-04-12 18:16:16 +0200

    Add project files.

---
+++ Semestr1_Lekcja10_2023/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Semestr1_Lekcja10_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //przypomnienie FOR
+
+            //wypisz liczby od 1 do 10 za pomocą pętli for
+
+            for(int x = 50; x >= 1; x = x - 2)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.Clear();
+
+            //powoduje wydanie dźwięku przez konsolę
+            //1 parametr częstotliwość dźwięku w hercach (hz)
+            //w parametr jak długo dźwięk ma być odtworzony (ms)
+
+            for(int x = 100; x <= 10000; x = x + 100)
+            {
+                Console.Beep(x, 200);
+                Console.WriteLine($"Teraz odtwarzam dźwięk o częstotliwości {x}");
+            }
+
+            int czestotliwosc = 100;
+            while (czestotliwosc <= 10000)  //wykonuje się dopóki wyrażenie logiczne ma wartość true (prawda)
+            {
+                Console.Beep(czestotliwosc, 200);
+                Console.WriteLine($"Teraz odtwarzam dźwięk o częstotliwości {czestotliwosc}");
+                czestotliwosc = czestotliwosc + 100;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ While/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //używając pętli while wypisz wszystkie liczby podzeielne przez 3, od 0 do 1000
+
+            int x = 0;
+            while(x < 1000)
+            {
+                Console.WriteLine(x);
+                x = x + 3;
+            }
+
+            Console.Clear();
+
+            // napisz program, który wygeneruje klucze cd key w formie xxxxx-xxxxx-xxxxx-xxxxx-xxxxx
+            // użyj do tego pętli while wygeneruj 50 różnych kluczy
+            Random liczbaLosowa = new Random();
+            int y = 1;
+            while (y <= 10) {
+
+                string klucz = "";       // pusty klucz do którego dokleimy resztę cyfr
+                int dlugoscKlucza = 25;  // ustalamy długość klucza (ilość x)
+
+                int numerZnaku = 0;
+                while (numerZnaku < dlugoscKlucza)
+                {
+                    klucz = klucz + liczbaLosowa.Next(0, 10);   // dodawanie losowych cyfr do cd-keya
+                    numerZnaku++;        // zwiększanie zmiennej startowej
+
+                    // dodawnie pauz co 5 miejsc i zabezpiecznie przed dodaniem pauzy na końcu
+                    if (numerZnaku % 5 == 0 && numerZnaku != dlugoscKlucza)
+                    {
+                        klucz = klucz + "-";
+                    }
+                }
+                Console.WriteLine($"Klucz nr {y}: {klucz}"); // wypisanie kompletnego klucza
+                y++;
+            }
+            Console.Clear();
+
+            //pętla do while zawsze wykona sie conajmniej raz niezależnie od warunku
+            do
+            {
+                Console.WriteLine("Test");
+            }
+            while (false);
+
+            //jeśli warunek będzie wartości true to pętla zachowa się jak zwykły while
+            int z = 1;
+            do
+            {
+                Console.WriteLine("Test");
+                z++;
+            }
+            while (z <= 10);
+
+
+            //break przerywa działanie pętli
+            for(int t = 0; t < 10; t++) 
+            {
+                if(t == 5)
+                {
+                    break;
+                }
+                Console.WriteLine($"t = {t}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
